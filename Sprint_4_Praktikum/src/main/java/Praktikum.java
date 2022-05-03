fcb9c12d14815b2a085c06de0b78fb851793aea8
Elizaveta <95237300+inwild86@users.noreply.github.com>
2022-05-03 17:46:48 +0400

    Delete Sprint_4_Praktikum directory

--- Sprint_4_Praktikum/src/main/java/Praktikum.java
+++
@@ -1,7 +0,0 @@
-public class Praktikum {
-    public static void main(String[] args) {
-        String name = "Оделий Воркута";
-        Account account = new Account(name);
-        account.checkNameToEmboss();
-    }
-}
