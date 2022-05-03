b02a1685fca2cc2f1bd9aa9e687613c3a16981c4
IW <lisa143.97@gmail.com>
2022-05-03 17:21:51 +0400

    add class

---
+++ Sprint_4_Praktikum/src/main/java/Account.java
@@ -0,0 +1,17 @@
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+
+        if (name == null) {
+            return false;
+        } else return name.matches("(?=.{3,19}$)[a-zA-Zа-яёА-ЯЁ]+\\s[a-zA-Zа-яёА-ЯЁ]+");
+
+    }
+
+}
---
+++ Sprint_4_Praktikum/src/main/java/Praktikum.java
@@ -0,0 +1,7 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        String name = "Оделий Воркута";
+        Account account = new Account(name);
+        account.checkNameToEmboss();
+    }
+}
